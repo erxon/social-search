865641870706e1f667786267aa1044562c7ce301
Ericson Castasus <castasuse@gmail.com>
2022-10-14 14:04:26 +0800

    remove logs

--- index.js
+++ index.js
@@ -1,7 +1,6 @@
 
 
 const { NlpManager } = require("node-nlp");
-console.log("Starting Chatbot ...");
 
 const manager = new NlpManager({ languages: ["en"] });
 manager.load();
@@ -9,13 +8,11 @@ manager.load();
 var readline = require("readline");
 var rl = readline.createInterface(process.stdin, process.stdout);
 
-console.log("Chatbot started!");
 rl.setPrompt("> ");
 rl.prompt();
 
 rl.on("line", async function (line){
     const response = await manager.process("en", line);
-    console.log(response.answer);
     rl.prompt();
 }).on("close", function (){
     process.exit(0);
